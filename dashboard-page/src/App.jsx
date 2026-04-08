49b8dc11f9d6af0a66a8dd905611c22ba39ed2b3
Michal Surazynski <michalsur@hotmail.com>
2026-04-08 21:06:27 +0200

    feat: display tasks data

--- dashboard-page/src/App.jsx
+++ dashboard-page/src/App.jsx
@@ -1,4 +1,5 @@
 import weatherData from "../../data/weather.json";
+import taskData from "../../data/tasks.json";
 const icons = import.meta.glob("./assets/icons/*.svg", { eager: true });
 
 function getIcon(code) {
@@ -37,7 +38,8 @@ function App() {
 
   return (
     <div className="absolute w-full h-full bg-background-sky">
-      <div className="my-16 mx-8">
+      <div className="my-16 mx-8 flex flex-col space-y-12">
+
         <div className="flex justify-center gap-8 bg-primary p-4 rounded-xl">
           {weatherData.map((entry) => (
             <div key={entry.hour} className="flex flex-col items-center">
@@ -51,7 +53,24 @@ function App() {
             </div>
           ))}
         </div>
+
+        <div className="flex flex-col justify-center gap-8 bg-primary p-8 rounded-xl">
+          {taskData.tasks.map((entry) => (
+            <div className="flex items-center space-x-6">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-check-big-icon lucide-circle-check-big"><path d="M21.801 10A10 10 0 1 1 17 3.335" /><path d="m9 11 3 3L22 4" /></svg>
+              <h2 className="text-text text-xl font-semibold">
+                {entry.title}
+              </h2>
+            </div>
+          ))}
+          {taskData["more-than-three"] ?
+            (<div className="w-full flex justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-icon lucide-ellipsis"><circle cx="12" cy="12" r="1" /><circle cx="19" cy="12" r="1" /><circle cx="5" cy="12" r="1" /></svg>
+            </div>) :
+            (<></>)}
+        </div>
       </div>
+
     </div>
   )
 }
