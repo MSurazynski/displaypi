152106963a42ddd7c6b242df4497ce7c47b946df
Michal Surazynski <michalsur@hotmail.com>
2026-04-09 16:26:20 +0200

    UI: refactor, add trash and date info

--- dashboard-page/src/App.jsx
+++ dashboard-page/src/App.jsx
@@ -1,6 +1,9 @@
 import weatherData from "../../data/weather.json";
 import taskData from "../../data/tasks.json";
 const icons = import.meta.glob("./assets/icons/*.svg", { eager: true });
+import { CircleCheck, Recycle, Ellipsis } from 'lucide-react'
+import StyledText from "./StyledText";
+
 
 function getIcon(code) {
   const name = WMO_TO_ICON[code];
@@ -38,36 +41,57 @@ function App() {
 
   return (
     <div className="absolute w-full h-full bg-background-sky">
-      <div className="my-16 mx-8 flex flex-col space-y-12">
+      <div className="my-16 mx-8 flex flex-col  space-y-4">
 
-        <div className="flex justify-center gap-8 bg-primary p-4 rounded-xl">
-          {weatherData.map((entry) => (
-            <div key={entry.hour} className="flex flex-col items-center">
-              <h2 className="text-text text-xl font-semibold">
+        <div className="flex justify-center mb-8!">
+          <StyledText size="large" font="bold">
+            Poniedziałek, 9 kwietnia
+          </StyledText>
+        </div>
+
+        <div className="flex justify-center gap-4 bg-primary p-4 rounded-3xl">
+          {weatherData.map((entry, index) => (
+            <div key={index} className="flex flex-col items-center">
+              <StyledText>
                 {entry.hour}:00
-              </h2>
-              <img src={getIcon(entry.weather_code)} className="w-22 h-22" />
-              <h2 className="text-text text-2xl font-bold">
+              </StyledText>
+              <img src={getIcon(entry.weather_code)} className="w-20 h-20" />
+              <StyledText size="large">
                 {entry.temp}°C
-              </h2>
+              </StyledText>
             </div>
           ))}
         </div>
 
-        <div className="flex flex-col justify-center gap-8 bg-primary p-8 rounded-xl">
-          {taskData.tasks.map((entry) => (
-            <div className="flex items-center space-x-6">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-check-big-icon lucide-circle-check-big"><path d="M21.801 10A10 10 0 1 1 17 3.335" /><path d="m9 11 3 3L22 4" /></svg>
-              <h2 className="text-text text-xl font-semibold">
+        <div className="flex flex-col justify-center gap-8 bg-primary p-4 rounded-3xl">
+          {taskData.tasks.map((entry, index) => (
+            <div key={index} className="flex items-center space-x-4">
+              <CircleCheck />
+              <StyledText>
                 {entry.title}
-              </h2>
+              </StyledText>
             </div>
           ))}
           {taskData["more-than-three"] ?
             (<div className="w-full flex justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-ellipsis-icon lucide-ellipsis"><circle cx="12" cy="12" r="1" /><circle cx="19" cy="12" r="1" /><circle cx="5" cy="12" r="1" /></svg>
+              <Ellipsis size={30} />
             </div>) :
             (<></>)}
+        </div>
+
+        <div className="grid grid-cols-2 gap-4">
+          <div className="flex flex-col justify-center items-center gap-4 p-4 rounded-3xl bg-primary">
+            <StyledText>
+              Papier
+            </StyledText>
+            <Recycle size={30} />
+            <StyledText>
+              10 kwietnia
+            </StyledText>
+          </div>
+
+          <div className="flex flex-col justify-center gap-8 p-6 rounded-3xl bg-primary">
+          </div>
         </div>
       </div>
 
