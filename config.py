ec6757e4feb60ce7760a3279c9afb04b345c96b3
Michal Surazynski <michalsur@hotmail.com>
2026-04-11 12:36:48 +0200

    refactor: clean up paths

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
-DASHBOARD_NOT_CONVERTED_IMAGE_NAME = Path("dashboard-screenshot.png")
-DASHBOARD_CONVERTED_IMAGE_NAME = Path("dashboard-screenshot.bmp")
-DASHBOARD_NOT_CONVERTED_DIRECTORY_PATH = Path("assets/images/temp")
-DASHBOARD_CONVERTED_DIRECTORY_PATH = Path("assets/images/converted/dashboard")
 DASHBOARD_PAGE_PROJECT_DIR = Path("dashboard-page")
 FALLBACK_IMAGE_DIRECTORY_PATH = Path("assets/images/fallback")
 
 TEMP_IMAGE_DIRECTORY_PATH = Path("assets/images/temp")
+
+DASHBOARD_CONVERTED_DIRECTORY_PATH = Path("assets/images/converted/dashboard")
+DASHBOARD_NOT_CONVERTED_IMAGE_NAME = Path("dashboard-screenshot.png")
+DASHBOARD_CONVERTED_IMAGE_NAME = Path("dashboard-screenshot.bmp")
+
+NASA_CONVERTED_IMAGE_DIRECTORY_PATH = Path("assets/images/converted/nasa")
 NASA_NOT_CONVERTED_IMAGE_NAME = Path("nasa_image.png")
 NASA_CONVERTED_IMAGE_NAME = Path("nasa_image.bmp")
