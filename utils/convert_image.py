12fe1967d89dbe65c0879a5b938de0ae0f80c928
Michal Surazynski <michalsur@hotmail.com>
2026-04-11 14:01:42 +0200

    fix: after adapting image rotate to match expected size

--- utils/convert_image.py
+++ utils/convert_image.py
@@ -48,6 +48,8 @@ def convert_image(image_to_convert_path: Path, output_directory_path: Path, use_
     # Now resize to exact display resolution
     img = img.resize((output_width, output_height))
 
+    img = img.transpose(Image.Transpose.ROTATE_270) # Rotate 270 degrees clockwise to match display orientation
+
     # Convert to the 6-color e-paper palette
     palette_img = Image.new("P", (1, 1))
     palette_img.putpalette([
