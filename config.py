a093b46877384e82e7cdacff081813f49389f0d9
Michal Surazynski <michalsur@hotmail.com>
2026-04-11 11:43:51 +0200

    feat: add config

---
+++ config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+DASHBOARD_IMAGE_NAME = Path("dashboard-screenshot.png")
+DASHBOARD_NOT_CONVERTED_DIRECTORY_PATH = Path("assets/images/temp")
+DASHBOARD_CONVERTED_DIRECTORY_PATH = Path("assets/images/converted/dashboard")
+DASHBOARD_PAGE_PROJECT_DIR = Path("dashboard-page")
+FALLBACK_IMAGE_DIRECTORY_PATH = Path("assets/images/fallback")
