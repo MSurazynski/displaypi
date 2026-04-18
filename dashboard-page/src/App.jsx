1d6c73e3478c52db44fca5a4deaf64ab9136eb1c
Michal Surazynski <michalsur@hotmail.com>
2026-04-18 10:21:14 +0200

    UI: more margins for weather

--- dashboard-page/src/App.jsx
+++ dashboard-page/src/App.jsx
@@ -127,10 +127,13 @@ function App() {
         )}
 
         {/* pogoda */}
-        <div className="flex flex-col gap-2 px-4">
-          <StyledText align="left" size="large">
-            Pogoda
-          </StyledText>
+        <div className="flex flex-col">
+          <div className="flex flex-col gap-2 px-4">
+            <StyledText align="left" size="large">
+              Pogoda
+            </StyledText>
+          </div>
+
           <WeatherTemperatureChart data={weatherData} className="w-30 h-30" />
         </div>
 
