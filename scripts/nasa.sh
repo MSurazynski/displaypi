881110f6f517977156718c061fe1180600232b04
Michal Surazynski <michalsur@hotmail.com>
2026-04-18 22:03:33 +0200

    fix: make scritps work on both RPI and laptop

--- scripts/nasa.sh
+++ scripts/nasa.sh
@@ -2,7 +2,11 @@
 set -e
 
 export PATH="/usr/local/bin:/usr/bin:/bin"
-cd /home/michal/displaypi
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_DIR="$(cd -- "$SCRIPT_DIR/.." && pwd)"
+
+cd "$PROJECT_DIR"
 
 /usr/bin/just sync
 /usr/bin/just display-nasa
